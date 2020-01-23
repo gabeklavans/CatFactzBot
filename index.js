1ac4f4bbae3c22c196e496d25a4001ca4b0fcf37
Gabriel Klavans <gabeklav@bu.edu>
2020-01-23 12:55:23 -0500

    Added cat fact service

---
+++ index.js
@@ -0,0 +1,45 @@
+// jshint esversion: 6
+require('dotenv').config();
+const tgBot = require('node-telegram-bot-api');
+
+const bot = new tgBot(process.env.TOKEN, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+
+    bot.sendMessage(msg.chat.id, "Welcome");
+
+});
+
+bot.onText(/\/fact/, (msg) => {
+    var request = require("request");
+
+    var options = {
+        method: 'GET',
+        url: 'https://catfact.ninja/fact',
+        headers:
+        {
+            Host: 'catfact.ninja',
+        },
+        json: true
+    };
+
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+
+        bot.sendMessage(msg.chat.id, body.fact);
+    });
+
+});
+
+// Some other client. I would use it but it has pretty bad API documentation
+
+// const Telegraf = require('telegraf');
+
+// const bot = new Telegraf(process.env.TOKEN);
+// bot.start((ctx) => ctx.reply('Welcome!'));
+// bot.help((ctx) => ctx.reply('Send me a sticker'));
+
+// bot.command('echo', ctx => ctx.reply(`Did you say '${ctx.message.text}'?`));
+
+// bot.launch();
+// console.log('Bot launched!');

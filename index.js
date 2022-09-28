47983ed468da7954c33dacdc6c3c50e3cc2397fe
Gabriel Klavans <gabeklav@fastmail.com>
2022-09-28 15:15:02 -0400

    Clean up comments

--- index.js
+++ index.js
@@ -1,50 +1,26 @@
-// jshint esversion: 6
-require('dotenv').config();
-const tgBot = require('node-telegram-bot-api');
+require("dotenv").config();
+const tgBot = require("node-telegram-bot-api");
 
 const bot = new tgBot(process.env.TOKEN, { polling: true });
 // const FACT_URL = 'https://cat-fact.herokuapp.com/facts/random';
-const FACT_URL = 'https://catfact.ninja/fact';
+const FACT_URL = "https://catfact.ninja/fact";
 
 bot.onText(/\/start/, (msg) => {
-
-    bot.sendMessage(msg.chat.id, "Welcome");
-
+  bot.sendMessage(msg.chat.id, "Welcome");
 });
 
 bot.onText(/\/fact/, (msg) => {
-    var request = require("request");
+  var request = require("request");
 
-    var options = {
-        method: 'GET',
-        // url: 'https://catfact.ninja/fact',
-	url: FACT_URL,
-        headers:
-        {
-            //Host: 'catfact.ninja',
-	    //Host: 'cat-fact.herokuapp'
-        },
-        json: true,
-	//rejectUnauthorized: false
-    };
+  var options = {
+    method: "GET",
+    url: FACT_URL,
+    json: true,
+  };
 
-    request(options, function (error, response, body) {
-        if (error) throw new Error(error);
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
 
-        bot.sendMessage(msg.chat.id, body.fact);
-    });
-
+    bot.sendMessage(msg.chat.id, body.fact);
+  });
 });
-
-// Some other client. I would use it but it has pretty bad API documentation
-
-// const Telegraf = require('telegraf');
-
-// const bot = new Telegraf(process.env.TOKEN);
-// bot.start((ctx) => ctx.reply('Welcome!'));
-// bot.help((ctx) => ctx.reply('Send me a sticker'));
-
-// bot.command('echo', ctx => ctx.reply(`Did you say '${ctx.message.text}'?`));
-
-// bot.launch();
-// console.log('Bot launched!');
